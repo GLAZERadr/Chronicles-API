274786d1f78c80e7d6bf4b0b0cbd272c8f9e7d5d
GLAZERadr <adrianbadjideh@student.telkomuniversity.ac.id>
2024-05-22 22:19:29 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, {Application} from 'express';
+import { errorMiddleware } from './common/middlewares/error.middlewares';
 
 const app: Application = express();
 const port: number = parseInt(process.env.SERVER_PORT as string, 10) || 3000;
@@ -6,6 +7,11 @@ const baseUrl: string = process.env.BASE_URL || '/api';
 
 app.use(express.json());
 app.use(baseUrl);
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Server is listening in ${port}`);
